03a723c5bbd0e28d6209cf22c91103806c435e4b
JustinDosaj <justindosaj@gmail.com>
2025-03-15 16:59:34 -0700

    Updates

--- amplify/functions/chat/handler.ts
+++ amplify/functions/chat/handler.ts
@@ -1,21 +1,39 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
 import { secret } from "@aws-amplify/backend";
 import OpenAI from "openai";
 
-export const handler = async (): Promise<null> => {
+export const handler = async ( event: APIGatewayProxyEventV2 ): Promise<APIGatewayProxyResultV2> => {
 
-    const client = new OpenAI({apiKey: `${secret('OPENAI_API_KEY')}`})
+    try {
 
-    const completion = await client.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-            {
-                role: "user",
-                content: "Write a one-sentence bedtime story about a unicorn.",
-            },
-        ],
-    });
+        const client = new OpenAI({apiKey: `${secret('OPENAI_API_KEY')}`})
+        
+        console.log(event)
 
-    console.log(completion)
+        const completion = await client.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+                {
+                    role: "user",
+                    content: "Write a one-sentence bedtime story about a unicorn.",
+                },
+            ],
+        });
 
-    return null;
+        console.log(completion)
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ response: completion.choices[0].message.content }),
+          };
+
+    } catch (error) {
+        console.log("Error: ", error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Something went wrong" }),
+        };
+    }
+
+
 };
