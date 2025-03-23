3a3455a9f8ebcc325a271835e0e0adadf4686f3f
JustinDosaj <justindosaj@gmail.com>
2025-03-23 12:17:37 -0700

    Track String Length removePersonalnfo
    1. Added way to track length of string based on difference of word occurrence and it's replacement to accurately replacement words in upcoming loops

--- amplify/functions/remove/handler.ts
+++ amplify/functions/remove/handler.ts
@@ -2,13 +2,19 @@ import dotenv from "dotenv";
 import { Schema } from "../../data/resource";
 import { ComprehendClient, DetectPiiEntitiesCommand, LanguageCode } from "@aws-sdk/client-comprehend";
 
+
+// PROBLEM: .replace() does NOT replace all matching words --> only the first one it finds
+// SOLUTION 1: Find all words from entity list first 
+// --> create new object/map that has all word occurance locations 
+// --> inside current loop, check object for multiple occurances
+// --> If multiple occurances exist, loop through that and keep doing .replace() until all replaced
+// SOLUTION 2: Just keep creating new vars for each name regardless of duplicates? --> inefficient
 export const handler: Schema["removePersonalInfo"]["functionHandler"] = async (event) => {
 
-    dotenv.config()
-    const client = new ComprehendClient({region: 'us-east-2'})
-
-    // Try Comprehend
     try {
+
+        dotenv.config()
+        const client = new ComprehendClient({region: 'us-east-2'})        
         
         let message: string = event.arguments.message || ''
 
@@ -24,19 +30,28 @@ export const handler: Schema["removePersonalInfo"]["functionHandler"] = async (e
 
         console.log("Entities: ", entities)
         
-        // John went to the store for Frank
-
+        // Track difference in string length based on replacements
+        let diff = 0
+        
+        // Loop through every entity to find replacement words
         for (let i = 0; i < length; i++) {
-            
-            console.log(`Message before alteration: ${message}`)
 
-            const start: number = entities[i].BeginOffset || 0
-            const end: number = entities[i].EndOffset || 0
+            // Define start and end of word to be replaced
+            const start: number = (entities[i].BeginOffset || 0) + diff
+            const end: number = (entities[i].EndOffset || 0) + diff
             const type = entities[i].Type
 
-            const substring = message.substring(start, end + 1)
+            // Find the string and construct the replacement
+            const original = message.substring(start, end) 
+            const replacement = ` [${type}_${i}] `
             
-            message = message.replace(substring, ` [${type}_${i}] `)
+            // Calculate difference in length to re-calculate start/end next loop iteration
+            const origLen = original.length
+            const replaceLen = replacement.length
+            diff += replaceLen - origLen
+
+            // Replace all original words with thier replacements
+            message = message.replace(original, replacement)
         }
 
         return { content: message }
