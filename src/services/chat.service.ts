769d054fc31ac5506a81dd452a9cc079dc6af2e7
JustinDosaj <justindosaj@gmail.com>
2025-03-28 13:41:46 -0700

    Fix API Key Error

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,19 +1,19 @@
-// import { generateClient } from "aws-amplify/api"
-// import { Schema } from "../../amplify/data/resource";
-import OpenAI from "openai";
+import { generateClient } from "aws-amplify/api"
+import { Schema } from "../../amplify/data/resource";
+//import OpenAI from "openai";
 
 interface ChatProps {
     message: string,
     privacySettings: Record<string, boolean>
 }
 
-const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_API_KEY, dangerouslyAllowBrowser: true});
+//const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_API_KEY, dangerouslyAllowBrowser: true});
 
 export const sendChatMessage = async ({message, privacySettings}: ChatProps) => {
 
     console.log(privacySettings)
 
-    //const client = generateClient<Schema>({ authMode: 'userPool' })
+    const client = generateClient<Schema>({ authMode: 'userPool' })
 
     try {
         
@@ -22,29 +22,29 @@ export const sendChatMessage = async ({message, privacySettings}: ChatProps) => 
         //     settings: JSON.stringify(privacySettings)
         // })
 
-        // const response = await client.queries.chatCompletion({
-        //     message: message
-        // })
+        const response = await client.queries.chatCompletion({
+            message: message
+        })
 
-        const completion = await openai.chat.completions.create({
-            messages: [
-                {
-                    role: "system",
-                    content: "Respond in Markdown format recongizable by React Markdown with remarkGfm"
-                },
-                { 
-                    role: "user", 
-                    content: message 
-                }
-            ],
-            model: "gpt-3.5-turbo",
-        });
+        // const completion = await openai.chat.completions.create({
+        //     messages: [
+        //         {
+        //             role: "system",
+        //             content: "Respond in Markdown format recongizable by React Markdown with remarkGfm"
+        //         },
+        //         { 
+        //             role: "user", 
+        //             content: message 
+        //         }
+        //     ],
+        //     model: "gpt-3.5-turbo",
+        // });
 
-        const content = completion.choices[0].message.content
+        // const content = completion.choices[0].message.content
 
-        const response = { data: { content: content } }
+        // const response = { data: { content: content } }
 
-        console.log(response)
+        // console.log(response)
 
         return response.data?.content || ''
 
