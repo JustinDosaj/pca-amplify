b274514c349bdfd5342859fd09c2b4349861ba09
JustinDosaj <justindosaj@gmail.com>
2025-04-08 12:17:33 -0700

    Reroute User
    1. Reroute user if they are on invalid route (ie. invalid conversationId, non-existing conversation-id)

--- src/hooks/useConversations.ts
+++ src/hooks/useConversations.ts
@@ -1,21 +1,29 @@
 import { useState } from "react";
 import { useAuth } from "./useAuth";
 import { getConversations } from "@/services/api.service";
+import { IConversationsList } from "@/types/chat";
 
 export const useConversations = () => {
 
     const { user } = useAuth();
-    const [ conversations, setConversations ] = useState(null)
+    const [ conversations, setConversations ] = useState<IConversationsList[]>([])
+    const [ conversationIdList, setConversationIdList] = useState<string[]>([])
 
     const fetchConversations = async () => {
-        if (user && conversations === null) {
+        if (user && conversations.length === 0) {
             const data = await getConversations({user})
+
+            // Extract and set all conversation ids to validate url param
+            const allIds = data.map((con: IConversationsList) => con.conversationId)
+            setConversationIdList([...conversationIdList, ...allIds])
+
             setConversations(data)
         }
     }
 
     return {
         conversations,
+        conversationIdList,
         fetchConversations
     }
 }
