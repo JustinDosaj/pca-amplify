730200b89b6c33b77815f3d20c607423960fe437
JustinDosaj <justindosaj@gmail.com>
2025-04-08 17:26:47 -0700

    Add New Chat Button

--- src/components/ui/chat/Menu.tsx
+++ src/components/ui/chat/Menu.tsx
@@ -2,6 +2,8 @@ import { IAppView } from "@/types/settings"
 import { IConversationsList } from "@/types/chat"
 import React from "react"
 import { useRouter } from "next/navigation"
+import { PlusIcon } from "@heroicons/react/24/solid"
+import { Button } from "../Button"
 
 interface IMenu extends IAppView {
     conversations: IConversationsList[] | null
@@ -13,13 +15,18 @@ export default function Menu({className, conversations}: IMenu) {
 
     return (
         <div className={`${className} border-r border-slate-300/40 p-6 bg-slate-50`}>
-            <h2 className="text-lg font-semibold text-slate-900 mb-4">Menu</h2>
+            <div className="flex justify-between items-center mb-4">
+                <h2 className="text-lg font-semibold text-slate-900">Menu</h2>
+                <Button onClick={() => router.replace('/chat/new')} color="white" variant="outline">
+                    <PlusIcon className="text-slate-900 h-5 w-5"/>
+                </Button>
+            </div>
             <div className="space-y-2">
                 {conversations?.map((item, index) => (
                 <div 
                     key={index} 
                     className="p-3 bg-white rounded-lg text-slate-900 shadow-sm hover:bg-slate-100 transition-colors cursor-pointer text-base"
-                    onClick={() => router.push(`/chat/${item.conversationId}`)}
+                    onClick={() => router.replace(`/chat/${item.conversationId}`)}
                 >
                     {item.title}
                 </div>
