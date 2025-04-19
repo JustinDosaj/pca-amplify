550bf3f76c2f60d3dc0f8d3e2b2682b2df5b071f
JustinDosaj <justindosaj@gmail.com>
2025-04-18 18:00:48 -0700

    Add Pricing Page

---
+++ src/app/(platform)/pricing/page.tsx
@@ -0,0 +1,18 @@
+
+"use client"
+
+import { Header } from "@/components/elements/Header";
+import { Footer } from "@/components/elements/Footer";
+
+export default function Pricing() {
+
+  return (
+    <>
+        <Header/>
+        <main className="h-screen mx-auto my-auto grid">
+            Pricing Page
+        </main>
+        <Footer/>
+    </>
+  );
+}
